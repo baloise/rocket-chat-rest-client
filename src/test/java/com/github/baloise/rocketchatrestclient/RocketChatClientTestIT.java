f101806c0070b032b4820ff284e9e0fa969883ed
Marco Descher <marco@descher.at>
2021-06-08 13:18:53 +0200

    Revert "#61 test direct post to channel"
    This reverts commit 03279cf003d20a4f8e56309f2cdf121686b1ef38.

--- src/test/java/com/github/baloise/rocketchatrestclient/RocketChatClientTestIT.java
+++ src/test/java/com/github/baloise/rocketchatrestclient/RocketChatClientTestIT.java
@@ -12,7 +12,6 @@ import org.junit.Test;
 
 import com.github.baloise.rocketchatrestclient.model.Channel;
 import com.github.baloise.rocketchatrestclient.model.Group;
-import com.github.baloise.rocketchatrestclient.model.Message;
 import com.github.baloise.rocketchatrestclient.model.Room;
 import com.github.baloise.rocketchatrestclient.model.ServerInfo;
 import com.github.baloise.rocketchatrestclient.model.Setting;
@@ -275,14 +274,6 @@ public class RocketChatClientTestIT {
 			fail(e.getMessage());
 		}
 
-	}
-
-	@Test
-	public void testPostMessageToChannel() throws IOException {
-		Channel[] list = this.rc.getChannelsApi().list();
-		assertTrue(list.length > 0);
-		Message message = new Message("test message");
-		this.rc.getChatApi().postMessage(list[0], message);
 	}
 
 }
